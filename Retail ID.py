230d666f51f7809611688d296d48c63497670bc3
Junyi_Lou <junyi_lou@outlook.com>
2016-09-14 22:06:28 +0800

    Kuaidi Update Candidate
    Kuaidi 20160914 is now a candidate 4.0 new version.

--- Retail ID.py
+++ Retail ID.py
@@ -2,6 +2,7 @@
 #rtl = "%03d" % j
 import os, sys, fileinput
 def down():
+	spr = "".join(["/R", rtl, ".png"])
 	exia = os.path.isfile("".join([fourbythree, rtl, ".png"]))
 	sx = "".join([sixbynine, rtl, ".png"])
 	exib = os.path.isfile(sx)
@@ -9,12 +10,12 @@ def down():
 	else: exic = False
 	if exic:
 		print "".join(["\nPhotos of R",rtl," already found, checking..."])
-		os.system("".join(["wget -q -t 3 -c ", dieter, "16_9/R", rtl, ".png"]))
-		prefix = "".join(["/Users/Junyi_Lou/R", rtl, ".png"])
+		os.system("".join(["wget -q -t 3 -c ", dieter, "16_9", spr]))
+		prefix = "".join(["/Users/Junyi_Lou",spr])
 		newsize = os.path.getsize(prefix)
 		oldsize = os.path.getsize(sx)
 		if newsize != oldsize and newsize > 81920:
-			os.system("".join(["mv -n ", sixbynine, rtl, ".png ", rpath, "/Other/R", rtl, ".png"]))
+			os.system("".join(["mv -n ", sixbynine, rtl, ".png ", rpath, "/Other", spr]))
 			os.system("".join(["rm ", fourbythree, rtl, ".png"]))
 			fb = open("".join([rpath, "/List.md"]))
 			newlist = fb.read().replace(("".join([rtl, ","])), "")
@@ -29,8 +30,9 @@ def down():
 	if not exic:
 		equa = ["4_3", "16_9"]
 		for k in range(0, 2):
-			os.system("".join(["wget -t 2 -c -P ", rpath, "/", equa[k], "/ ", dieter, equa[k], "/R", rtl, ".png"])) 
+			os.system("".join(["wget -t 2 -c -P ", rpath, "/", equa[k], "/ ", dieter, equa[k], spr])) 
 			k = k + 1
+		os.system("".join(["open ", sx]))
 arg = 0
 rpath = "/Users/Junyi_Lou/Downloads/Retail"
 sixbynine = "".join([rpath, "/16_9/R"])
