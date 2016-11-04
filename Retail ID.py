93c5553f859200de597941b35d0e38fa7d26d784
Junyi_Lou <junyi_lou@outlook.com>
2016-11-04 20:37:02 +0800

    Notes releaf

--- Retail ID.py
+++ Retail ID.py
@@ -14,17 +14,17 @@ def filesize(url): #通过HEAD分析远程文件大小
         return int(dict(response.headers).get('content-length', 0))
 def down(): #rtl样例 092
 	spr = "".join(["/R", rtl, ".png"]) #spr样例 /R092.png
-	exia = os.path.isfile("".join([fbt, rtl, ".png"])) #请求样例 /Users/Junyi_Lou/Downloads/Retail/4_3/R092.png
-	sx = "".join([sbn, rtl, ".png"]) #sx样例 /Users/Junyi_Lou/Downloads/Retail/16_9/R092.png
+	exia = os.path.isfile("".join([fbt, rtl, ".png"])) #请求样例 ~/Downloads/Retail/4_3/R092.png
+	sx = "".join([sbn, rtl, ".png"]) #sx样例 ~/Downloads/Retail/16_9/R092.png
 	exib = os.path.isfile(sx) #exia为4:3存在bool, exib为16_9
 	if exia and exib : exic = True
 	else: exic = False
 	if exic:
-		newsize = filesize("".join([dieter, "/16_9", spr])) #请求样例 http://rtlimages.apple.com/cmc/dieter/store/16_9/R092.png
+		newsize = filesize("".join([dieter, "/16_9", spr]))
 		oldsize = os.path.getsize(sx)
 		if newsize != oldsize and newsize > 409600:
 			os.system("".join(["mv -n ", sbn, rtl, ".png ", rpath, "Other", spr, " && rm ", fbt, rtl, ".png"]))
-			#请求样例 mv -n /Users/Junyi_Lou/Downloads/Retail/R092.png /Users/Junyi_Lou/Downloads/Retail/Other/R092.png && rm /Users/Junyi_Lou/Downloads/Retail/4_3/092.png
+			#请求样例 mv -n ~/Downloads/Retail/R092.png ~/Downloads/Retail/Other/R092.png && rm ~/Downloads/Retail/4_3/092.png
 			fb = open("".join([rpath, "List.md"]))
 			newlist = fb.read().replace(("".join([rtl, ","])), ""); fb.close() #注意,不能空格替换
 			fc = open("".join([rpath, "List.md"]), "w")
@@ -35,13 +35,13 @@ def down(): #rtl样例 092
 		equa = ["/4_3/", "/16_9/"]
 		for k in range(0, 2):
 			os.system("".join(["wget -t 2 -e \"http_proxy=http://127.0.0.1:6152\" -c -P ", rpath, equa[k], " ", dieter, equa[k], spr]))
-			#请求样例 wget -t(尝试次数) 2 -e(代理设置) "http_proxy=http://127.0.0.1:6152" -c(断点续传) -P(指定位置) /Users/Junyi_Lou/Downloads/Retail//16_9/ http://rtlimages.apple.com/cmc/dieter/store/16_9//R092.png
+			#请求样例 wget -t(尝试次数) 2 -e(代理设置) "http_proxy=http://127.0.0.1:6152" -c(断点续传) -P(指定位置) ~/Downloads/Retail//16_9/ http://rtlimages.apple.com/cmc/dieter/store/16_9//R092.png
 			k += 1
 		os.system("".join(["open ", sx]))
 arg = 0
 rpath = "/Users/Junyi_Lou/Downloads/Retail/"
-sbn = "".join([rpath, "16_9/R"]) #16:9 /Users/Junyi_Lou/Downloads/Retail/16_9/R
-fbt = "".join([rpath, "4_3/R"]) #4:3 /Users/Junyi_Lou/Downloads/Retail/4_3/R
+sbn = "".join([rpath, "16_9/R"]) #16:9 ~/Downloads/Retail/16_9/R
+fbt = "".join([rpath, "4_3/R"]) #4:3 ~/Downloads/Retail/4_3/R
 dieter = "http://rtlimages.apple.com/cmc/dieter/store"
 for m in sys.argv[1:]: arg += 1
 if arg == 0:
