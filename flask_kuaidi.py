e237424ebfbdc205cd902e3f1320d6c143eb1906
Junyi_Lou <junyi_lou@outlook.com>
2016-12-09 18:28:26 +0800

    Flask Candidate

--- flask_kuaidi.py
+++ flask_kuaidi.py
@@ -1,39 +1,43 @@
 # -*- coding:utf-8 -*-
-from __future__ import division
 from flask import Flask, jsonify, request
-import sys, json, urllib2, time
+import sys, json, urllib2, time, datetime
 app = Flask(__name__)
-#Flask needed. Created by Junyi_Lou
-#未来思路:
-#    参数: 最大条数 求min值 for循环最大数字
-#    前天 昨天显示 参考 "http://www.iplaypython.com/sys/s100.html"
-#    format 日期到 8月3日 18:23 格式
-#本地处理的部分:
-#    对于快递公司代号的翻译
-#    对于已签收的bool值反馈
-#    对于快件的备注显示
-@app.route('/kuaidi/', methods=['GET'])  
+@app.route('/', methods=['GET'])  
 def home():
     readid = request.args.get('id').__str__()
+    readmax = int(request.args.get('max').__str__())
     if readid != "None":
         urla = ''.join(["https://www.kuaidi100.com/autonumber/autoComNum?text=", readid])
-        comp = json.loads(urllib2.urlopen(urla).read())["auto"][0]["comCode"]
-        urlb = ''.join(["https://www.kuaidi100.com/query?type=", comp, "&postid=", readid])
-        responce = urllib2.urlopen(urlb)
-        anst = responce.read(); ansj = json.loads(anst)
-        if ansj["status"] == "200": #成功
-            erstat = 1
-            maxnum = anst.count("location")
-            result = ansj["data"]
-            tasks = list(range(maxnum+1))
-            tasks [0] = {'signature': erstat, 'postid': ansj["nu"],'comp': ansj["com"],'timestamp': int(time.time())} 
-            for i in range (1, maxnum+1): tasks [i] = {'time': result[i-1]["time"],'content':result[i-1]["context"]}
-        else: #下载失败
+        countp = urllib2.urlopen(urla).read().count("comCode")
+        if (countp-1):
+            comp = json.loads(urllib2.urlopen(urla).read())["auto"][0]["comCode"]
+            urlb = ''.join(["https://www.kuaidi100.com/query?type=", comp, "&postid=", readid])
+            responce = urllib2.urlopen(urlb)
+            anst = responce.read(); ansj = json.loads(anst) #"Thu, 08 Dec 2016 00:00:00 GMT"
+            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%m月%d日")
+            fronteday = (datetime.datetime.now() - datetime.timedelta(days=2)).strftime("%m月%d日")
+            comtext = {'yuantong': '圆通', 'yunda': '韵达', 'shunfeng': '顺丰', 'shentong': '申通', 'zhongtong': '中通'}
+            if ansj["status"] == "200":
+                erstat = 1
+                maxnum = min(anst.count("location"),readmax)
+                result = ansj["data"]
+                tasks = list(range(maxnum+1))
+                tasks [0] = {'signature': erstat, 'postid': ansj["nu"],'comp': ''.join([comtext.get(ansj["com"],"其他"),"快递"]),'timestamp': int(time.time())} 
+                for i in range (1, maxnum+1):
+                    ResultTime = result[i-1]["time"]
+                    StrfTime = time.strftime("%m月%d日 %H:%M", time.strptime(ResultTime, "%Y-%m-%d %H:%M:%S"))
+                    result[i-1]["time"] = StrfTime.replace(yesterday,"昨天").replace(fronteday,"前天")
+                for j in range (1, maxnum+1): 
+                    tasks [j] = {'time': result[j-1]["time"],'content':result[j-1]["context"]}
+            else:
+                tasks = list(range(1))
+                tasks [0] = {'signature': 0, 'content': ansj["message"]}
+        else:
             tasks = list(range(1))
-            tasks [0] = {'signature': 0, 'content': ansj["message"]}
-    else: #URL失败
+            tasks [0] = {'signature': 0, 'content': r"找不到单号对应公司"}
+    else:
         tasks = list(range(1))
         tasks [0] = {'signature': 0, 'content': r"URL没有指定单号"}
     return jsonify(tasks)
 if __name__ == '__main__':  
-    app.run()
+    app.run(host='0.0.0.0',port=6555,debug=False)
