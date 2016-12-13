50072b9426641e95be49b4fb5ba2b939096a7bc9
Junyi_Lou <junyi_lou@outlook.com>
2016-12-13 22:02:06 +0800

    2016 1214 Happy Xiaomi Network

--- Text Exchange.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-import os, sys
-os.system("clear")
-print "输入需要替换为空的文本"
-exc = raw_input()
-arg = 0
-for m in sys.argv[1:]: arg = arg + 1
-for j in range(1, arg + 1): os.system(''.join(["mv -n ", '"', sys.argv[j], '" "', sys.argv[j].replace(exc, ""), '"']))
--- iPhone Chongqing.py
+++
@@ -1,5 +0,0 @@
-# -*- coding:utf-8 -*-
-import urllib2,urllib,os,json; equa = ["R480", "R476","R573"]; eqn = 3
-for p in range(0, eqn): 
-	k = json.loads(urllib2.urlopen(urllib2.Request("http://ir.weip.tech/Home/GetStoreiPhoneList",urllib.urlencode({'storecode':equa[p],'regioncode':'CN','onlyshowavailability':'true'}))).read())
-	print k; p += 1
