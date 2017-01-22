3d9b8a0f622b8ba9269a2fef9386c319c2573cc6
Junyi Lou <junyi_lou@outlook.com>
2017-01-22 23:47:15 +0800

    Retail ID

--- RetailID_Instapush.py
+++ RetailID_Instapush.py
@@ -1,4 +1,4 @@
-import os, sys, fileinput, urllib2, datetime
+import os, sys, fileinput, urllib2, datetime, json, time
 def filesize(url):
     opener = urllib2.build_opener()
     request = urllib2.Request(url)
@@ -12,10 +12,12 @@ def filesize(url):
         return int(dict(response.headers).get('content-length', 0))
 def down(rtl):
 	tmnow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	spr = "R" + rtl + ".png"
-	sx = sbn + rtl + ".png"
+	spr = "R" + rtl + ".png"; sx = sbn + rtl + ".png"
 	exi = os.path.isfile(sx)
 	newsize = filesize(dieter + "/16_9/" + spr)
+	nameopen = open(rpath + "name.md")
+	storejson = json.loads(nameopen.read())
+	nameopen.close()
 	if exi: oldsize = os.path.getsize(sx)
 	else: oldsize = 0
 	if newsize != oldsize and newsize > 409600:
@@ -25,11 +27,12 @@ def down(rtl):
 		fc.write(newlist); fc.close()
 		a='curl -X POST -H "x-instapush-appid: '; b='" -H "x-instapush-appsecret: '
 		c='" -H "Content-Type: application/json" -d '; d="'"
-		e='{"event":"Retail","trackers":{"rtl":"'; f=rtl
-		g='","size":"'; h=str(newsize / 1024)+"KB"; m='"}'; n='}'
-		o="'"; p=' https://api.instapush.im/v1/post'
+		e='{"event":"retail","trackers":{"rtl":"'; f=rtl
+		g='","size":"'; h=str(newsize / 1024)+"KB"; i='","name":"';
+		j=storejson[0][rtl]; m='"}'; n='}'; o="'"; 
+		p=' https://api.instapush.im/v1/post'
 		AppID = "585e4e62a4c48a05d607b545"; AppSecret = "a32883f25245516940ea6b9f9b80fa54"
-		finalOut = a+AppID+b+AppSecret+c+d+e+es+f+g+h+m+n+o+p
+		finalOut = a+AppID+b+AppSecret+c+d+e+f+g+h+i+j+m+n+o+p
 		os.system(finalOut)
 		os.system("sudo rm " + sbn + rtl + ".png")
 		os.system("sudo wget -t 2 -c -P " + rpath + " " + dieter + "/16_9/" + spr)
@@ -37,8 +40,11 @@ def down(rtl):
 rpath = "/home/pi/Retail/"
 sbn = rpath + "R"
 dieter = "https://rtlimages.apple.com/cmc/dieter/store"
-for line in fileinput.input(rpath + "List.md"):
+while True:
+	for line in fileinput.input(rpath + "List.md"):
 		for j in range (0, line.count(",")):
 			rtl = (line.split(","))[j]
 			down(rtl)
-print
+	for t in range(1,12):
+		print "Sleeping. " + str(12 - t) + " hours out of 12 hours left."
+		time.sleep(3600)
