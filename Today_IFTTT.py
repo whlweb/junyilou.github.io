2fd4dd21ceb7af517de4addee24305e5c83abd12
Junyi Lou <junyilou@users.noreply.github.com>
2017-06-26 10:13:01 -0500

    Atom len()

--- Today_IFTTT.py
+++ Today_IFTTT.py
@@ -6,9 +6,8 @@ filename = ['nanjingeast', 'kunming', 'wangfujing', 'taikoolichengdu', 'riversid
 			'thaihotplaza', 'olympia66dalian', 'zhongjiejoycity', 'jiefangbei', 'ifcmall', 'galaxymacau', 'taipei101']
 cityname = ['上海', '昆明', '北京', '成都', '天津', '济南', '青岛', '广州', '深圳', '南宁', '南京', '无锡',
 			'郑州', '杭州', '厦门', '福州', '大连', '沈阳', '重庆', '香港特别行政区', '澳门特别行政区', '台湾']
-# 添加城市需要修改 22 数字，或者在下次修改代码时直接改用 len 函数
-checksum = list(range(22))
-for c in range(0, 22): checksum[c] = 0
+checksum = list(range(len(cityname)))
+for c in range(0, len(cityname)): checksum[c] = 0
 def home():
 	wAns = ""; wCount = 0; nowDatetime = datetime.datetime.now(); rpath = os.path.expanduser('~') + "/Retail/"
 	os.system("rm " + rpath + "*.json"); os.system("wget -t 0 -T 3 -P " + rpath + " --no-check-certificate -i " + rpath + "url.md"); os.system("clear")
@@ -24,7 +23,7 @@ def home():
 				pushAns = "Apple 在" + cityname[i] + "有新活动 " + rJson[lct]["shortName"]
 				singleURL = rJson[lct]["image"]; print pushAns
 				if nCheck == 0:
-					os.system('curl -X POST -H "Content-Type: application/json" -d' + "'" + '{"value1":"' + pushAns 
+					os.system('curl -X POST -H "Content-Type: application/json" -d' + "'" + '{"value1":"' + pushAns
 							   + '", "value2":"' + singleURL + '"}' + "' https://maker.ifttt.com/trigger/today/with/key/dJ4B3uIsxyedsXeQKk_D3x"); print
 				# GitHub users please notice: IFTTT Key only uses for private.
 				wAns = wAns + singleID + ", "; wCount += 1; checksum[i] += 1
