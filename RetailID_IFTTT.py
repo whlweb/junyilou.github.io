685fe663f11776077a26d4f962e2b19083493cff
Junyi Lou <junyilou@users.noreply.github.com>
2017-07-04 00:52:36 -0500

    RetailID Exit

--- RetailID_IFTTT.py
+++ RetailID_IFTTT.py
@@ -7,7 +7,7 @@ def filesize(url):
 	try: response = opener.open(request); response.read()
 	except Exception, e: return 0
 	else: return int(dict(response.headers).get('content-length', 0))
-def down(rtl):
+def down(rtl, ps):
 	spr = "/R" + rtl + ".png"; sx = sbn + rtl + ".png"
 	exi = os.path.isfile(sx); newsize = filesize(dieter + "/16_9" + spr)
 	nameopen = open("/home/pi/Retail/name.md"); storejson = json.loads(nameopen.read()); nameopen.close()
@@ -18,19 +18,24 @@ def down(rtl):
 		pushRaw = "零售店图册 - Apple " + storejson[0][rtl] + " 刚刚获得了更新，店号 R" + rtl + "，图片大小 " + str(newsize / 1024) + " KB，访问 Apple 官网了解更多。"
 		if exi: os.system("mv -n " + sbn + rtl + ".png " + rpath + "Other/previous/"); exi = False
 		if not exi: os.system("wget -t 2 -c -P " + rpath + "Pictures/ " + dieter + "/16_9" + spr)
-		os.system('curl -X POST -H "Content-Type: application/json" -d' + "'" + '{"value1":"' + pushRaw + '"}'
-			   + "' https://maker.ifttt.com/trigger/raw/with/key/dJ4B3uIsxyedsXeQKk_D3x"); print
-		os.system('curl -X POST -H "Content-Type: application/json" -d' + "'" + '{"value1":"' + pushRaw + '"}' 
-			   + "' https://maker.ifttt.com/trigger/raw/with/key/bOGI8iEAyvjh782UYFKbRa"); print
+		#os.system('curl -X POST -H "Content-Type: application/json" -d' + "'" + '{"value1":"' + pushRaw + '"}'
+		#	   + "' https://maker.ifttt.com/trigger/raw/with/key/dJ4B3uIsxyedsXeQKk_D3x"); print
+		#os.system('curl -X POST -H "Content-Type: application/json" -d' + "'" + '{"value1":"' + pushRaw + '"}' 
+		#	   + "' https://maker.ifttt.com/trigger/raw/with/key/bOGI8iEAyvjh782UYFKbRa"); print
 		# GitHub users please notice: IFTTT key only uses for private.
+		sys.argv[ps] = ""
 	else: print "Checked R" + rtl +" has no update, ignore."
 rpath = "/home/pi/Retail/"; sbn = rpath + "Pictures/R"
 dieter = "https://rtlimages.apple.com/cmc/dieter/store"; arg = 0
 sTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 for m in sys.argv[1:]: arg += 1
 while True:
+	arm = 0
 	#os.system("clear")
 	#for j in range(713, 714): down("%03d" % j)
-	for j in range(1, arg + 1): down(sys.argv[j])
+	for j in range(1, arg + 1): down(sys.argv[j], j)
+	for k in range(1, arg + 1):
+		if sys.argv[k] == "": arm += 1
+	if arm == arg: break
 	print "Sleeping, interval will be 2hr."; time.sleep(7200)
 print "\nStarted: " + sTime + "\nEnded:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
