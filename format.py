bb1ccec5e444814bb985f5e777107cb73ad011ac
Junyi Lou <junyilou@users.noreply.github.com>
2017-07-31 21:04:48 +0800

    useless args

--- format.py
+++ format.py
@@ -1,6 +1,4 @@
 import json, sys
 from collections import OrderedDict
-arg = 0
-for m in sys.argv[1:]: arg += 1
 f = open(sys.argv[1]); fr = f.read(); f.close()
 print json.dumps(json.loads(fr, object_pairs_hook = OrderedDict), ensure_ascii = False, indent = 2)
