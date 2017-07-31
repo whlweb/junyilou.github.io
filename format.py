4ddf1fc2640ae7f80e58c72cb79da2dbc73d9676
Junyi Lou <junyilou@users.noreply.github.com>
2017-07-31 21:03:25 +0800

    Order

--- format.py
+++ format.py
@@ -1,5 +1,6 @@
 import json, sys
+from collections import OrderedDict
 arg = 0
 for m in sys.argv[1:]: arg += 1
 f = open(sys.argv[1]); fr = f.read(); f.close()
-print json.dumps(json.loads(fr), ensure_ascii = False, indent = 2)
+print json.dumps(json.loads(fr, object_pairs_hook = OrderedDict), ensure_ascii = False, indent = 2)
