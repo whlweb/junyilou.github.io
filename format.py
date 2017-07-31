21845f782c2b485c39fadd7e654fcd60497fdba7
Junyi Lou <junyilou@users.noreply.github.com>
2017-07-31 20:59:58 +0800

    Unicode

--- format.py
+++ format.py
@@ -2,4 +2,4 @@ import json, sys
 arg = 0
 for m in sys.argv[1:]: arg += 1
 f = open(sys.argv[1]); fr = f.read(); f.close()
-print json.dumps(json.loads(fr), indent = 4)
+print json.dumps(json.loads(fr), ensure_ascii = False, indent = 2)
