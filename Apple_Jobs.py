6232cca8ed2c8b4c50f2381baade942d69b20d98
Junyi_Lou <junyi_lou@outlook.com>
2018-02-04 17:11:45 +0800

    Jobs and Name

--- Apple_Jobs.py
+++ Apple_Jobs.py
@@ -1,16 +1,18 @@
 #-*- coding:utf-8 -*-
-import os, json, filecmp, platform, time, signal
+import os, json, filecmp, platform, time
 
 if "Linux" in platform.platform(): tilde = os.path.expanduser('~') + "/Retail/"
 if "Darwin" in platform.platform(): tilde = os.path.expanduser('~') + "/Downloads/Apple/Raspberry/"
 preDir = tilde + "Jobs/"
 
-#def emergency(a, b): exit()
 def cRep(org, dep, des = ""): return org.replace(dep, des)
 def wget(post, url, savename):
-	os.system('wget -t 0 -T 3 -O ' + tilde + savename
+	os.system('wget -t 0 -T 4 -O ' + tilde + savename
 		+ ' --no-check-certificate --post-data "countryCode=CHN&stateCode='
 		+ post + '" https://jobs.apple.com/cn/location' + url)
+def push(pushRaw):
+	os.system("wget -t 0 -T 8 --no-check-certificate --post-data 'value1=" 
+		+ pushRaw + "' https://maker.ifttt.com/trigger/raw/with/key/dJ4B3uIsxyedsXeQKk_D3x")
 
 def down():
 	sOpen = open(tilde + "states.json")
@@ -20,6 +22,7 @@ def down():
 		lOpen = open(tilde + "cities" + str(s) + ".json"); lRead = lOpen.read(); lOpen.close()
 		if "<!DOCTYPE html>" in lRead:
 			print "Apple Jobs is now having an update.\nPlease check jobs information later.\n"
+			push("[招贤纳才]Apple 招聘页面开始维护了，监测将退出。")
 			os.system("rm " + preDir + "cities*.json"); exit()
 	check("cities")
 	for c in range(0, len(sJson)):
@@ -58,40 +61,29 @@ def dl_fix(fileName, byp = 0):
 		lcA = int(cRep(cRep(cRep(fileName, "location"), ".json"), "-" + lcB))
 		cOpen = open(tilde + "cities" + str(lcA) + ".json"); cJson = json.loads(cOpen.read()); cOpen.close()
 		wget(str(sJson[lcA]["id"]) + "&cityCode=" + cJson[int(lcB)]["cityName"], ".json", fileName)
-	if byp == 0: print "Not a location or city file."
+	if byp == 0: print "Not a location nor city file."
 
 def compare():
+	changeOut = noChange = p = c = ""
 	for files in os.walk(preDir):
 		for l in range(0, len(files[2])):
-			oldLoc = preDir + files[2][l]; newLoc = cRep(oldLoc, "/Jobs"); p = ""
+			oldLoc = preDir + files[2][l]; newLoc = cRep(oldLoc, "/Jobs")
 			if files[2][l][0] != "." and files[2][l][-5:] == ".json":
 				if filecmp.cmp(oldLoc, newLoc) == False:
 					oldOpen = open(oldLoc); oldJson = len(json.loads(oldOpen.read())); oldOpen.close()
 					newOpen = open(newLoc); newJson = len(json.loads(newOpen.read())); newOpen.close()
 					os.system("mv " + newLoc + " " + newLoc.replace(os.path.basename(newLoc), os.path.basename(newLoc).replace(".json", "-1.json")))
-					if oldJson < newJson: p = "had  " + str(newJson) + " items, instead of " + str(oldJson) + " now."
-					if oldJson == newJson: p = "has a text update without numbers updates."
-					if oldJson > newJson and newJson > 0: p = "seems to have a stop hiring."
-					if oldJson > newJson and newJson == 0: p = "stopped hiring."
-					print "[*] The location for file '" + os.path.basename(oldLoc) + "' " + p
-				else: print "Checked file '" + os.path.basename(oldLoc) + "' has no update."
+					if oldJson < newJson: p = "had  " + str(newJson) + " items, instead of " + str(oldJson) + " now"; c = "从原有的 " + str(oldJson) + "个招聘地点增加为 " + str(newJson) + " 个"
+					if oldJson == newJson: p = "has a text update without numbers updates"; c = "更改了部分文字，但地点数量没有变化"
+					if oldJson > newJson and newJson > 0: p = "seems to have some stop hiring"; c = "似乎停止了部分地点的招聘"
+					if oldJson > newJson and newJson == 0: p = "stopped hiring"; c = "不再招聘了"
+					changeOut =  "[*] The location for file '" + os.path.basename(oldLoc) + "' " + p
+					push("[招贤纳才]Apple 招聘文件 " + os.path.basename(oldLoc) + "所代表的地点" + c + "。")
+				else: noChange = noChange + "Checked file '" + os.path.basename(oldLoc) + "' has no update.\n"
+	if changeOut == "": changeOut = "No changes found in this check."
+	print noChange + "\n============\n" + changeOut
 	os.system("mv -f " + tilde + "cities*.json " + preDir)
 	os.system("mv -f " + tilde + "location*.json " + preDir)
+	time.sleep(86400)
 
-def custom():
-	while True:
-		newSuzhou = tilde + "Suzhou.json"; os.system("touch " + newSuzhou); oldSuzhou = preDir + "Suzhou.json"
-		newChengdu = tilde + "Chengdu.json"; os.system("touch " + newChengdu); oldChengdu = preDir + "Chengdu.json"
-		while os.path.getsize(newSuzhou) < 650: wget("165&cityCode=Suzhou", ".json", "Suzhou.json")
-		while os.path.getsize(newChengdu) < 1300: wget("178&cityCode=Chengdu", ".json", "Chengdu.json")
-		mRead = open(newSuzhou); maintain = mRead.read()
-		if "<!DOCTYPE html>" in maintain: os.system("wget -t 0 -T 8 --no-check-certificate --post-data 'value1=[招贤纳才]Apple 招聘页面开始维护了。' https://maker.ifttt.com/trigger/raw/with/key/dJ4B3uIsxyedsXeQKk_D3x"); exit()
-		else:
-			if filecmp.cmp(oldSuzhou, newSuzhou) == False:
-				os.system("wget -t 0 -T 8 --no-check-certificate --post-data 'value1=[招贤纳才]Apple 在苏州的招聘计划似乎发生了改变。' https://maker.ifttt.com/trigger/raw/with/key/dJ4B3uIsxyedsXeQKk_D3x"); exit()
-			if filecmp.cmp(oldChengdu, newChengdu) == False:
-				os.system("wget -t 0 -T 8 --no-check-certificate --post-data 'value1=[招贤纳才]Apple 在成都的招聘计划似乎发生了改变。' https://maker.ifttt.com/trigger/raw/with/key/dJ4B3uIsxyedsXeQKk_D3x"); exit()
-		print "Sleeping, interval will be 12 hr."; os.system("rm " + newSuzhou); os.system("rm " + newChengdu); time.sleep(43200)
-
-#signal.signal(signal.SIGINFO, emergency)
-custom() #down(); compare()
+down(); compare()
