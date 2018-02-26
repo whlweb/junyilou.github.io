ac2e9c49bbde6eadc0b142c26a6a456cfa0dc292
Junyi_Lou <junyi_lou@outlook.com>
2018-02-26 23:17:18 +0800

    keyNotice()

--- Kuaidi_IFTTT.py
+++ Kuaidi_IFTTT.py
@@ -1,9 +1,20 @@
 # -*- coding:utf-8 -*-
 import sys, json, urllib2, time, datetime, os, signal, exceptions
 
-arg = signCheck = siging = brew = tti = forTime = 0; nt = ""
+arg = signCheck = siging = brew = tti = forTime = 0; nt = ""; keyBool = False
 endl = "\n"; argv = list(range(10)); masterKey = "dJ4B3uIsxyedsXeQKk_D3x"
 
+def keyNotice():
+	osLogName = os.environ.get("LOGNAME"); global keyBool
+	if osLogName != "junyi_lou" and osLogName != "pi" and masterKey == "dJ4B3uIsxyedsXeQKk_D3x" and not keyBool:
+		print "=================================="
+		print "警告: 这似乎不是 Junyi Lou 的电脑，但 IFTTT Key 并未在源代码中修改。"
+		print "为个人方便未在源代码中删除自用 Master Key，继续将会把消息推送至我的设备。"
+		print "您不仅无法体验本文件的功能，同时还将对我造成困扰。如需注册 IFTTT Key，"
+		print "请打开 README.md (https://junyilou.github.io) 并参照加粗文字。"
+		print "=================================="
+		keyBool = input("\n确定要继续么？",)
+	if not keyBool: exit()
 def plut(pint):
 	if (pint - 1): plural = "s"
 	else: plural = ""
@@ -13,8 +24,9 @@ def netTry(tryurl):
 	try: response = urllib2.urlopen(tryurl)
 	except urllib2.URLError: return "False"
 	else: return response.read()
-def pushbots(pushRaw): os.system("wget -t 0 -T 3 --no-check-certificate --post-data 'value1=" + pushRaw + "' https://maker.ifttt.com/trigger/raw/with/key/" + masterKey)
-	# GitHub users please notice: IFTTT key only uses for private.
+def pushbots(pushRaw): 
+	keyNotice()
+	os.system("wget -t 0 -T 3 --no-check-certificate --post-data 'value1=" + pushRaw + "' https://maker.ifttt.com/trigger/raw/with/key/" + masterKey)
 def autocomp(readid):
 	aTry = netTry("https://www.kuaidi100.com/autonumber/autoComNum?text=" + readid)
 	if aTry != "False":
