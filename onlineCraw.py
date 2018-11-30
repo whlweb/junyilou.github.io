d70b8eaf22bccc2babe06026dfe3b1ea17635262
Junyi_Lou <junyi_lou@outlook.com>
2018-11-30 23:53:44 +0800

    bugfix and improvement

--- onlineCraw.py
+++ onlineCraw.py
@@ -1,5 +1,5 @@
 #-*- coding:utf-8 -*-
-import urllib2, os, sys, time
+import urllib2, os, sys, time, ssl
 from BeautifulSoup import BeautifulSoup
 
 alphabet = [chr(i) for i in range(65,91)]
@@ -11,8 +11,9 @@ ans = list()
 def title(partno):
 	reload(sys); sys.setdefaultencoding('utf-8')
 	url = "https://www.apple.com/cn/shop/product/" + partno
-	soup = BeautifulSoup(urllib2.urlopen(url))
-	return partno + " " + soup.title.string.replace(" - Apple (中国)", "").replace("购买 ", "")
+	try: soup = BeautifulSoup(urllib2.urlopen(url, timeout = 30))
+	except ssl.SSLError: return "[获取产品名称超时]"
+	else: return soup.title.string.replace(" - Apple (中国)", "").replace(" - Apple", "").replace("购买 ", "")
 
 for k in range(0, len(psbhd)):
 	for i in range(0, len(flist)):
@@ -20,33 +21,46 @@ for k in range(0, len(psbhd)):
 			slct = psbhd[k] + flist[i] + flist[j]
 			ans.append('M' + slct + '2')
 
-kOpen = open(os.path.expanduser('~') + "/MU.txt")
-kRead = kOpen.read(); kOpen.close()
-pushAns = ""; runtim = 0
+runtim = 0
+isKey = os.path.isfile(os.path.expanduser('~') + "/key.txt")
+if not isKey:
+	print ("Please provide your IFTTT key in ~/key.txt\n" +
+	"Location of the txt can be edited in the source code."); exit()
+else: 
+	kOpen = open(os.path.expanduser('~') + "/key.txt")
+	masterKey = list()
+	for line in open(os.path.expanduser('~') + "/key.txt"):
+		line = kOpen.readline().replace("\n", "")
+		masterKey.append(line)
+	kOpen.close()
 
 while True:
-	runtim += 1
+	runtim += 1; runnot = "[" + str(runtim) + "] "
+	mOpen = open(os.path.expanduser('~') + "/MU.txt")
+	mRead = mOpen.read(); mOpen.close()
 	for a in range(0, len(ans)):
 		url = 'https://www.apple.com/cn/shop/product/' + ans[a]
-		try: p = urllib2.urlopen(url, timeout=5)
+		try: p = urllib2.urlopen(url, timeout = 20)
+		except ssl.SSLError: 
+			print runnot + ans[a] + " 500 [" + str(a + 1) + "/" + str(len(ans)) + "]\r",
+ 			sys.stdout.flush()
 		except urllib2.HTTPError: 
-			print str(runtim) + " " + ans[a] + " 404 [" + str(a) + "/" + str(len(ans)) + "]\r",
+			print runnot + ans[a] + " 404 [" + str(a + 1) + "/" + str(len(ans)) + "]\r",
 			sys.stdout.flush()
 		else: 
-			if ans[a] in kRead: 
-				print str(runtim) + " " + ans[a] + " 302 [" + str(a) + "/" + str(len(ans)) + "]\r",
+			if ans[a] in mRead: 
+				print runnot + ans[a] + " 302 [" + str(a + 1) + "/" + str(len(ans)) + "]\r",
 				sys.stdout.flush()
 			else:
-				kWrite = open(os.path.expanduser('~') + "/MU.txt", "w"); kWrite.write(kRead + ans[a] + "\n"); kWrite.close();
-				pushAns = pushAns + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " " + ans[a] + "\n"
-				os.system("wget -t 100 -T 5 --no-check-certificate --post-data 'value1=重要的事情说三遍：新产品！！这一次先不告诉你是什么东西你赶紧看看，再不看就买不到了！！" 
-				+ "&value2=https://www.apple.com/cn/home/images/heroes/holiday/holiday_hero_1c_medium_2x.jpg"
-				+ "&value3=" + url + "' https://maker.ifttt.com/trigger/linkraw/with/key/d8xzMnWyRiM9cI4i0Zz6Cm")
-				os.system("wget -t 100 -T 5 --no-check-certificate --post-data 'value1=重要的事情说三遍：新产品！！新产品的 Part Number 是 " + ans[a] + "，马上买买买！！" 
-				+ "&value2=https://www.apple.com/cn/home/images/heroes/holiday/holiday_hero_1c_medium_2x.jpg"
-				+ "&value3=" + url + "' https://maker.ifttt.com/trigger/linkraw/with/key/d8xzMnWyRiM9cI4i0Zz6Cm")
-				os.system("wget -t 100 -T 5 --no-check-certificate --post-data 'value1=重要的事情说三遍：新产品！！新产品是 " + title(ans[a])
-				+ "&value2=https://www.apple.com/cn/home/images/heroes/holiday/holiday_hero_1c_medium_2x.jpg"
-				+ "&value3=" + url + "' https://maker.ifttt.com/trigger/linkraw/with/key/d8xzMnWyRiM9cI4i0Zz6Cm")
-	print pushAns
-	time.sleep(60)
+				print "\nNew Product Found: " + ans[a] + " at " + str(a + 1) + "/" + str(len(ans))
+				picURL = ("https://as-images.apple.com/is/image/AppleInc/aos/published/images" + 
+				"/M/" + ans[a][:2] + "/" + ans[a] + "/" + ans[a] + "?fmt=jpg")
+				pausea = input("等待确认A")
+				hWrite = open(os.path.expanduser('~') + "/MU.txt", "w"); hWrite.write(mRead + ans[a] + "\n"); hWrite.close();
+				pauseb = input("等待确认B")
+				for p in range(0, 2):
+					os.system("wget -t 100 -T 5 --no-check-certificate --post-data 'value1=Apple Online Store 更新了新产品：" 
+					+ title(ans[a]) + " " + ans[a] + " (第" + str(p + 1) + "次推送)" + "&value2=" + picURL + "&value3=" + url 
+					+ "' https://maker.ifttt.com/trigger/linkraw/with/key/" + masterKey[0])
+				os.system("rm -f " + masterKey[0] + "*")
+	time.sleep(1800)
